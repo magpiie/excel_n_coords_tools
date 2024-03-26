484e8cef42a9e5ff314da000cf693ea1c519b548
JeongJun Jo <rgfdds98.dev@gmail.com>
2024-03-26 12:20:33 +0900

    Feat: geodata converting fn. ver1
    coords행에 sdo객체 형태로 값 저장
    - 그러나 이 형태로 바로 insert할 수 있는 건 아니라 query생성과정에서 다시 가공 필요 => 즉, 불필요한 작업이었음
    - 또한 5186 변환 과정에서 정확한 변환이 이뤄지고 있지 않아서 geoserver와의 데이터 소통 문제있음
      - 좌표 범위 이상
      - 지도에는 표기되나 클릭 지점 데이터 못 가져옴

---
+++ addr_to_geodata/convert_geodata_v1.js
@@ -0,0 +1,50 @@
+import { readDataFromExcel, saveDataToExcel } from "../excel_tools";
+import {
+  convertAddressToCoordinates,
+  convertCoordinatesFromWGS84,
+} from "../coords_tools";
+
+const sourceExcelPath = process.env.TARGET_FILE_PATH; // 원본 엑셀 파일 경로
+const resultExcelPath = process.env.RESULT_FILE_PATH; // 결과 엑셀 파일 경로
+
+// 저장할 엑셀 헤더 정보
+const excelHeaderInfo = ["id", "name", "address", "coords"];
+
+// 메인 로직
+let originCoordsSystem = 4326;
+let targetCoordsSystem = 5186;
+async function main() {
+  const data = await readDataFromExcel(sourceExcelPath);
+  const results = [];
+
+  for (const item of data) {
+    const address = item["법정동 주소"];
+    const coord = await convertAddressToCoordinates(address);
+    if (coord) {
+      const [convertedX, convertedY] = convertCoordinatesFromWGS84(
+        originCoordsSystem,
+        targetCoordsSystem,
+        [(parseFloat(coord.x), parseFloat(coord.y))]
+      );
+      results.push({
+        id: item["ID"],
+        name: item["고시원명"],
+        address: address,
+        coords: JSON.stringify([
+          2001,
+          targetCoordsSystem,
+          [convertedX, convertedY, null],
+          [null],
+          [null],
+        ]),
+      });
+    } else {
+      console.log(`주소를 변환할 수 없습니다: ${address}`);
+    }
+  }
+
+  saveDataToExcel(excelHeaderInfo, results, resultExcelPath);
+  console.log("작업 완료");
+}
+
+main().catch(console.error);
